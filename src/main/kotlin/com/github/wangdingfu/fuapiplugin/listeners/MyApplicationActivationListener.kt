edf46d6843aea1c1639991e36c1e6b28b5651262
GitHub Action <action@github.com>
2024-08-09 05:28:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/wangdingfu/fuapiplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/wangdingfu/fuapiplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.wangdingfu.fuapiplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
